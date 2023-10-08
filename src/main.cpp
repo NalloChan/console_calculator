c9e4784b3cf60b2623802b1cedce7e990c427fc4
NalloChan <75219485+NalloChan@users.noreply.github.com>
2023-10-09 04:51:26 +0900

    Update: cpplintのテスト用にコードを追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 
 #include <iostream>
+#include <random>
 
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
+
+    std::uniform_int_distribution<int> dist(0, 1);
+    auto random_device = std::mt19937{std::random_device{}()};
+    auto num = dist(random_device);
+
+    if (num == 0)
+    {
+        std::cout << "Number is 0" << std::endl;
+    }
+    else
+    {
+        std::cout << "Number is other than 0" << std::endl;
+    }
+
     return 0;
 }
